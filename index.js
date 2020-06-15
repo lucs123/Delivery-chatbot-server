cfc19dea44c42e2480b989a90a476fb33727f2af
lucas <lucasf854@gmail.com>
2020-06-15 16:55:09 -0300

    add post

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000
 
+app.use(express.json())
+
 app.get('/',(req,res)=>{
 	res.send('Hello')
+	console.log('live');
+})
+
+app.post('/',(req,res)=>{
+	console.log(req.body);
+	res.send('Received')
 })
 
 app.listen(PORT)
