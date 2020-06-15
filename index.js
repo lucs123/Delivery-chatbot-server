e1b01caf86926cdb7c0497dcafed17e8e531fff3
lucas <lucasf854@gmail.com>
2020-06-15 16:47:43 -0300

    change port

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
+const PORT = process.env.PORT || 5000
 
 app.get('/',(req,res)=>{
 	res.send('Hello')
 })
 
-app.listen(3000)
+app.listen(PORT)
