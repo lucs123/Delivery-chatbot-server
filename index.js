d3b9e3ec151a6ff1de9cd2f83bd0f30955ec34e8
lucas <lucasf854@gmail.com>
2020-06-18 14:12:10 -0300

    bot pronto

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ class Order {
 					pedido: '',
 					valor: null,
 					formaEntrega:'',
+					endereco:'',
 					status: '' 
 					};
 	}
@@ -31,10 +32,10 @@ class Order {
 			let sabor = menu[this.sabores[i]]	
 			this.pizzas.push(sabor)
 			let str = this.quantidade[i].toString()+' pizza '+sabor.Pizza+', '
+			this.pedido.pedido = this.pedido.pedido.concat(str)
 			res = res.concat(str)
 		}
 		res = res + 'correto?(s/n)'
-		this.pedido.pedido = res
 		return (this.textResponse(res))
 	}
 
@@ -43,12 +44,20 @@ class Order {
 		for (var i = this.pizzas.length - 1; i >= 0; i--) {
 			value = value + this.pizzas[i].Valor
 		}
+		this.pedido.valor = value
 		let formatter = new Intl.NumberFormat('pt-BR', 
 				{
 				  style: 'currency',
 				  currency: 'BRL',
 				});
-		return (this.textResponse('O valor do pedido foi:'+formatter.format(value)+', é para entrega ou retirada?'))
+		value = formatter.format(value)
+		this.pedido.valor = value
+		return (this.textResponse('O valor do pedido foi:'+value+', é para entrega ou retirada?'))
+	}
+
+	formaEntrega = (forma,endereco) =>{
+		this.pedido.formaEntrega = forma
+		this.pedido.endereco = endereco
 	}
 
 	textResponse = (message)=>{
@@ -68,20 +77,47 @@ class Order {
 
 app.post('/',(req,res)=>{
 	console.log(req.body);
-	// console.log(order_response(sabores_,quantidade));
 	if(req.body.queryResult.intent.displayName === 'Pedido'){
 		const sabores_ = req.body.queryResult.parameters.sabor
 		const quantidade = req.body.queryResult.parameters.number
+
 		const sabores = sabores_.map(sabor=>(
 		slugify(sabor)))
+		
 		order = new Order(sabores,quantidade) 
 		res.send(order.orderResponse())
 	}
 	if(req.body.queryResult.intent.displayName === 'formaEntrega'){
 		res.send(order.billingResponse())
+		console.log(order.pedido);
 	}
-}
-)
+
+	if(req.body.queryResult.intent.displayName === 'entrega'){
+		order.formaEntrega('entrega',req.body.queryResult.parameters.endereco)
+		res.send(order.textResponse(
+			'Deseja confirmar seu pedido de '+order.pedido.pedido+' no valor de '+order.pedido.valor+
+			' para entrega em '+order.pedido.endereco+'?'))
+	}
+	if(req.body.queryResult.intent.displayName === 'retirada'){
+		order.formaEntrega('retirada','')
+		res.send(order.textResponse(
+			'Deseja confirmar seu pedido de '+order.pedido.pedido+' no valor de '+order.pedido.valor+
+			' para retirada?'))
+		console.log(order.pedido);
+	}
+	if(req.body.queryResult.intent.displayName === 'confirma-entrega'){
+		order.pedido.status = 'na fila'
+		console.log(order.pedido);
+		res.send(order.textResponse('Obrigado, seu pedido será entregue'))
+	}
+	if(req.body.queryResult.intent.displayName === 'confirma-retirada'){
+		order.pedido.status = 'na fila'
+		order.pedido.status = 'para retirada'
+		console.log(order.pedido);
+		res.send(order.textResponse('Obrigado, seu pedido será entregue'))
+	}		
+})
+
 
 function slugify (str) {
     var map = {
