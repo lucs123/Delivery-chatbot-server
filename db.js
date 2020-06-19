723d1af6f14cc6241e50cb18b8be5a5f9960cd53
lucs123 <lucasf854@gmail.com>
2020-06-19 20:42:55 -0300

    connect heroku database

--- db.js
+++ db.js
@@ -1,26 +1,17 @@
 const Pool = require('pg').Pool
-require('dotenv').config();
-const pool = new Pool()
 
-// class Queries{
-// 	insert = (params) =>{
-// 		pool.query('INSERT INTO pedidos(pedido,valor,formaEntrega,endereco,status) VALUES($1,$2,$3,$4,$5)',params, 
-// 			(err, res) => {
-// 			if (err) {
-// 		    	throw err
-// 		  		}
-// 		}
-// 		)
-// 	}
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
-
-// pool.query(('SELECT * FROM pedidos'), (err, res) => {
-//   if (err) {
-//     throw err
-//   }
-//   console.log(res.rows)
-// }
-// )
+// pool.query('SELECT * FROM pedidos;'
+// 	 , (err, res) => {
+//   if (err) throw err;
+//   console.log(res.rows);
+// });
 
 module.exports = pool
 
