f11c4d9ba507b391ec07cedd05461163512a90fd
lucs123 <lucasf854@gmail.com>
2020-06-27 19:53:03 -0300

    routes

---
+++ routes/chat.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const router = express.Router();
+const Order = require('../Bot.js')
+const slugify = require('../slugify.js')
+
+router.post('/',(req,res)=>{
+	console.log(req.body);
+	if(req.body.queryResult.intent.displayName === 'Pedido'){
+		const sabores_ = req.body.queryResult.parameters.sabor
+		const quantidade = req.body.queryResult.parameters.number
+
+		const sabores = sabores_.map(sabor=>(
+		slugify(sabor)))
+		
+		order = new Order(sabores,quantidade) 
+		res.send(order.orderResponse())
+	}
+	if(req.body.queryResult.intent.displayName === 'formaEntrega'){
+		res.send(order.billingResponse())
+		console.log(order.pedido);
+	}
+
+	if(req.body.queryResult.intent.displayName === 'entrega'){
+		order.formaEntrega('entrega',req.body.queryResult.queryText)
+		res.send(order.textResponse(
+			'Deseja confirmar seu pedido de '+order.pedido.pedido+' no valor de '+order.pedido.valor+
+			' para entrega em '+order.pedido.endereco+'?'))
+	}
+	if(req.body.queryResult.intent.displayName === 'retirada'){
+		order.formaEntrega('retirada','')
+		res.send(order.textResponse(
+			'Deseja confirmar seu pedido de '+order.pedido.pedido+' no valor de '+order.pedido.valor+
+			' para retirada?'))
+		console.log(order.pedido);
+	}
+	if(req.body.queryResult.intent.displayName === 'confirma-entrega'){
+		order.pedido.status = 'na fila'
+		console.log(order.pedido);
+		res.send(order.textResponse('Obrigado, seu pedido será entregue'))
+		order.finishOrder()
+	}
+	if(req.body.queryResult.intent.displayName === 'confirma-retirada'){
+		order.pedido.status = 'na fila'
+		order.pedido.status = 'para retirada'
+		console.log(order.pedido);
+		res.send(order.textResponse('Obrigado, seu pedido será entregue'))
+		order.finishOrder()
+	}		
+})
+
+
+module.exports = router
---
+++ routes/pedidos.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();	
+const pool = require('../db.js')
+
+router.get('/',(req,res)=>{
+	pool.query(('SELECT * FROM pedidos'), (err, results) => {
+	  	if (err) {
+	  	  throw err
+	  	}
+	  	console.log(results.rows);
+	  	res.send(results.rows)
+		}
+	)
+})	
+
+module.exports = router
