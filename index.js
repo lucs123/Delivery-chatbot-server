f0a16e0fa69eb0d954e99f386d2551d61a3f7923
lucs123 <lucasf854@gmail.com>
2020-07-07 11:52:45 -0300

    err function

--- index.js
+++ index.js
@@ -18,6 +18,11 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+	}
 });
 
 
