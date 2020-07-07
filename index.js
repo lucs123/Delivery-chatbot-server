72f25092d2e39237845110bc907812a13bd8d811
lucs123 <lucasf854@gmail.com>
2020-07-07 13:05:24 -0300

    fix err

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  function(err) {
+  (err) => {
     if (err) {
       res.status(500).send(err)
     }
