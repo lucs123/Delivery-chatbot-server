ec1e5f8a7a9ab84d4b7ea1e2b747a270cc092294
lucs123 <lucasf854@gmail.com>
2020-07-06 21:50:32 -0300

    changed contvariables declaration

--- routes/chat.js
+++ routes/chat.js
@@ -19,6 +19,8 @@ function getContext(req) {
 	return context.parameters	
 }
 
+let contVariables;
+
 router.post('/',async (req,res)=>{	
 
 	switch(req.body.queryResult.intent.displayName){
@@ -33,29 +35,29 @@ router.post('/',async (req,res)=>{
 			break;
 
 		case('formaEntrega'):
-			let contVariables = await getContext(req)
+			contVariables = await getContext(req)
 			let valores = contVariables.valores;
 			res.send(order.billingResponse(valores));
 			break;
 
 		case('entrega'):
-			let contVariables = await getContext(req)
+			contVariables = await getContext(req)
 			res.send(order.formaEntrega('entrega',req.body.queryResult.queryText, contVariables))
 			break;
 
 		case('retirada'):
-			let contVariables = await getContext(req)
+			contVariables = await getContext(req)
 			res.send(order.formaEntrega('retirada','Para retirada', contVariables))
 			break;
 		
 		case('confirma-entrega'):
-			let contVariables = await getContext(req)
+			contVariables = await getContext(req)
 			let deliverMessage = await order.finishOrder('Obrigado, seu pedido será entregue', contVariables)
 			res.send(deliverMessage)
 			break;
 
 		case('confirma-retirada'):
-			let contVariables = await getContext(req)
+			contVariables = await getContext(req)
 			let retrieveMessage = await order.finishOrder('Obrigado, seu pedido será preparado para retirada', contVariables)
 			res.send(retrieveMessage)
 			break;
