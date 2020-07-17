4f78b4cf20bccf0608dc26ac4aed497bd7125d8b
Lucas <lucasf854@gmail.com>
2020-07-17 20:19:41 -0300

    basic authentication

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('../db.js')
+const bcrypt = require('bcryptjs')
 
-router.post('/',(req,res)=>{
+router.post('/', async (req,res)=>{
 	console.log(req.body)
-	res.send({a:'received'})
+	
+	const{rows} = await pool.query('SELECT * FROM users')
+	const user = await rows[0]
+	console.log(user)
+	
+	if(req.body.email !== user.email){
+		return res.status(400).send({error:'Email inválido'})
+	}
+	
+	const validPass = await bcrypt.compare(req.body.password, user.password)
+	console.log(validPass)
+	
+	if(validPass){
+		return res.send({user:true})
+	}
+
+	else{		
+		return res.status(400).send({error:'Senha inválida'})
+	}	 
 })
+
 
 module.exports = router
