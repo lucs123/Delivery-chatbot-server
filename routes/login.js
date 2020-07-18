e466450688fee5068b1c9e30091b02feb50615e9
Lucas <lucasf854@gmail.com>
2020-07-17 21:50:56 -0300

    jwt

--- routes/login.js
+++ routes/login.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
+require('dotenv').config()
 const pool = require('../db.js')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 router.post('/', async (req,res)=>{
 	console.log(req.body)
 	
 	const{rows} = await pool.query('SELECT * FROM users')
 	const user = await rows[0]
-	console.log(user)
 	
 	if(req.body.email !== user.email){
 		return res.status(400).send({error:'Email inválido'})
@@ -18,7 +19,9 @@ router.post('/', async (req,res)=>{
 	console.log(validPass)
 	
 	if(validPass){
-		return res.send({user:true})
+		const token = jwt.sign({id_: user.id}, 
+			process.env.TOKEN_SECRET)
+		return res.header('auth-token', token).send({token:token})
 	}
 
 	else{		
